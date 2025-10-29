df2886082b716595a1ac790f8346fe060000a3b1
shuangxuetwelve <shuangxue12@outlook.com>
2025-10-29 13:52:12 -0400

    Do not integrate Langfuse on Github

--- run_docu_cat_github.py
+++ run_docu_cat_github.py
@@ -1,15 +1,9 @@
 import os
 from agents import agent_docu_cat_github
-from langfuse.langchain import CallbackHandler
-import uuid
 
-
-langfuse_handler = CallbackHandler()
-langfuse_session_id = uuid.uuid4()
 
 def main():
     """Main entry point for the action."""
-    event_name = os.getenv('GITHUB_EVENT_NAME')
     pr_number = os.getenv('PR_NUMBER')
     repository = os.getenv('GITHUB_REPOSITORY')
     token = os.getenv('GITHUB_TOKEN')
@@ -27,8 +21,7 @@ def main():
         "head_sha": head_sha,
     }
 
-    print(f"Calling the agent with Langfuse session ID: {str(langfuse_session_id)}")
-    result = agent_docu_cat_github.invoke(initial_state, config={"callbacks": [langfuse_handler], "metadata": {"langfuse_session_id": str(langfuse_session_id)}})
+    result = agent_docu_cat_github.invoke(initial_state)
     print(result)
 
 if __name__ == '__main__':
